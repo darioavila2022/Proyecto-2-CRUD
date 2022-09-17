7df56725167f5ea4db92d118cb1416da4dc7921b
Darío Ávila <108626018+darioavila2022@users.noreply.github.com>
2022-09-17 17:46:56 -0300

    Edición final. Finalización de JS y CSS.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Proyecto-2-CRUD
+
+ENLACE: 
+
+En este proyecto se trata de demostrar las capacidades de JavaScript en las 4 áreas fundamentales descritas en el acrónimo CRUD. Esta página trata de emular la noticia de un periódico, poniendo énfasis en una versión muy simple de una sección de comentarios.
+La información ingresada en el área de texto se guarda en localStorage en forma de string ocupando la función de JSON stringify, luego se invoca nuevamente a través de la función parse.
+La información guardada en el localStorage no se borra al ingresar una nueva, sino que las nuevas se van agregando a las antiguas.
+
+Descripción de prototipado simple:
+
+<img src="./prototipadoSimple.png">
--- main.js
+++ main.js
@@ -2,52 +2,55 @@ let form = document.getElementById("form");
 let input = document.getElementById("input");
 let msg = document.getElementById("msg");
 let posts = document.getElementById("posts");
+data = [];
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("button clicked");
-  
     formValidation();
-  });
-  
-  let formValidation = () => {
+});
+
+let formValidation = () => {
     if (input.value === "") {
-      msg.innerHTML = "Recuerda dar ideas!";
-      console.log("failure");
+        msg.innerHTML = "Recuerda dar ideas!";
     } else {
-      console.log("successs");
-      msg.innerHTML = "";
-      acceptData();
+        msg.innerHTML = "";
+        acceptData();
     }
-  };
-
-
-  let data = {};
-
-let acceptData = () => {
-    data["text"] = input.value;
-    console.log(data);
-    createPost();
 };
 
-let createPost = () => {
-    posts.innerHTML += `
-    <div>
-      <p>${data.text}</p>
-      <span class="options">
-        <i onClick="editPost(this)" class="fas fa-edit"></i>
-        <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
-      </span>
-    </div>
-    `;
+let acceptData = () => {
+    data = [...data, { "text": input.value }];
     input.value = "";
-  };
+    localStorage.setItem('ideaData', JSON.stringify(data));
+    provideIdea(data[data.length - 1]);
+};
 
-  let deletePost = (e) => {
+
+let provideIdea = (t) => {
+    let idea = document.createElement("div");
+    idea.innerHTML = `<p>${t.text}</p>
+    <span class="options"> 
+    <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+    <i onClick="editPost(this)" class="fas fa-edit"></i>
+      </span>`;
+    posts.append(idea);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    const ref = localStorage.getItem('IdeasContent');
+    if (ref) {
+        data = JSON.parse(ref);
+        data.forEach(t => {
+            provideIdea(t);
+        });
+    }
+});
+
+let deletePost = (e) => {
     e.parentElement.parentElement.remove();
-  };
-   
-  let editPost = (e) => {
+};
+
+let editPost = (e) => {
     input.value = e.parentElement.previousElementSibling.innerHTML;
     e.parentElement.parentElement.remove();
-  };
+};
